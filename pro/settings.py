66bb9caa52398270976dc596aa3ac9bb065b13b8
gautamgit676 <gautamgit244@gmail.com>
2025-12-31 12:56:25 +0530

    first second

--- pro/settings.py
+++ pro/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",  # add this
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
