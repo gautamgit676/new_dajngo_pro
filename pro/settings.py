10def00f83ee310f7b4c277c160a2f3f26e6f4aa
gautamgit676 <gautamgit244@gmail.com>
2026-01-05 16:15:27 +0530

    database adds

--- pro/settings.py
+++ pro/settings.py
@@ -98,21 +98,23 @@ SESSION_COOKIE_SECURE = True
 
 import os
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
-# if os.environ.get("DATABASE_URL"):
-#     DATABASES = {
-#         "default": dj_database_url.config(
-#             conn_max_age=600,
-#             ssl_require=True,
-#         )
-#     }
-# else:
-DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
+else:
+    DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": BASE_DIR / "db.sqlite3",
+            }
+        }
 
 # DATABASES = {
 #     'default': {
